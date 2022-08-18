d27ca81f8404595c3d558001d6767fec2c5ab68e
Akash N O <akash@armino.in>
2022-08-18 23:11:42 +0530

    added new icons

--- pages/index.vue
+++ pages/index.vue
@@ -4,92 +4,95 @@
     class="wrapper mx-auto p-4 flex items-end bg-primary align-middle h-screen w-screen bg-cover bg-no-repeat"
     style="background-image: url('lofi2.gif')"
   >
+          
     <div class="flex justify-between w-full align-middle items-center mx-auto">
-      <div style="min-width: 130px"></div>
+<span  class="cursor-pointer bg-gray-800 p-2  rounded-lg">
+            <img   src="pencil.png" width="15" alt=""  />
+          </span>
       <div class="flex-grow flex justify-center">
-        <audio ref="raining" src="rain.mp3"></audio>
-        <audio ref="typing" src="keyboard.mp3"></audio>
-        <audio
-          loop
-          ref="player"
-          src="https://s3.us-east-2.amazonaws.com/lofi.co/lofi.co/tracks/chill/chill_4.mp3"
-        ></audio>
-        <span @click="togglePlay()" class="cursor-pointer">
-          <img v-if="!isPlaying" src="play.png" alt="" class="w-12" />
+        <audio loop ref="primary" :src="audio"></audio>
+        <audio loop ref="type" src="keyboard.mp3"></audio>
+        <audio loop ref="rain" src="rain.mp3"></audio>
+        <!-- Primary play / pause button -->
+        <span @click="playPause('primary')" class="cursor-pointer">
+          <img v-if="!primary.playing" src="play.png" alt="" class="w-12" />
           <img v-else src="pause.png" alt="" class="w-12" />
         </span>
       </div>
+      <!-- Volume button -->
       <div class="flex space-x-4 align-middle">
         <span class="cursor-pointer">
           <div
             class="h-16 flex relative justify-end align-middle items-center"
-            @mouseenter="showVolume = true"
-            @mouseleave="showVolume = false"
+            @mouseenter="primary.showSlider = true"
+            @mouseleave="primary.showSlider = false"
           >
             <input
-              v-if="showVolume"
+              v-if="primary.showSlider"
               id="default-range"
               type="range"
               min="0"
               max="1"
-              v-model="volume"
+              @input="slideVolume('primary')"
+              v-model="primary.volume"
               step="0.01"
               class="rs-range w-full left-2 absolute h-2 bottom-16"
               orient="vertical"
             />
-            <span @click="toggleMute()">
-              <img v-if="!isMute" src="sound.png" width="30" alt="" />
-              <img v-else src="mute.png" width="30" alt="" />
+            <span @click="toggleMute()" class="bg-gray-800 p-2  rounded-lg">
+            <img   src="sound.png" width="15" alt="" :class="{'opacity-50 ':primary.muted}" />
             </span>
           </div>
         </span>
+        <!-- Keyboard button -->
         <div
           class="h-16 flex justify-end align-middle relative items-center"
-          @mouseenter="showTyping = true"
-          @mouseleave="showTyping = false"
+          @mouseenter="type.showSlider = true"
+          @mouseleave="type.showSlider = false"
         >
           <input
-            v-if="showTyping"
+            v-if="type.showSlider"
             id="default-range"
             type="range"
             min="0"
             max="1"
-            v-model="typing"
+            @input="slideVolume('type')"
+            v-model="type.volume"
             step="0.01"
             class="rs-range w-full left-2 absolute h-2 bottom-16"
             orient="vertical"
           />
-          <span @click="toggleTyping()" class="cursor-pointer">
-            <img v-if="isTyping" src="typing.png" width="30" alt="" />
-            <img v-else src="noTyping.png" width="30" alt="" />
+          <span @click="playPause('type')" class="cursor-pointer bg-gray-800 p-2  rounded-lg">
+            <img   src="typing.png" width="15" alt="" :class="{'opacity-50':!type.playing }" />
           </span>
         </div>
 
+        <!-- Rain cloud button -->
         <div
           class="h-16 flex justify-end align-middle relative items-center"
-          @mouseenter="showRaining = true"
-          @mouseleave="showRaining = false"
+          @mouseenter="rain.showSlider = true"
+          @mouseleave="rain.showSlider = false"
         >
           <input
-            v-if="showRaining"
+            v-if="rain.showSlider"
             id="default-range"
             type="range"
             min="0"
             max="1"
-            v-model="raining"
+            @input="slideVolume('rain')"
+            v-model="rain.volume"
             step="0.01"
             class="rs-range w-full left-2 absolute h-2 bottom-16"
             orient="vertical"
           />
-          <span @click="toggleRaining()" class="cursor-pointer">
-            <img v-if="isRaining" src="rain.png" width="30" alt="" />
-            <img v-else src="noRain.png" width="30" alt="" />
+          <span @click="playPause('rain')" class="cursor-pointer bg-gray-800 p-2  rounded-lg">
+            <img   src="rain.png" width="17" alt="" :class="{'opacity-50':!rain.playing }" />
           </span>
         </div>
-
+        <!-- Full Screen button -->
         <div class="h-16 flex justify-end align-middle relative items-center">
-          <span @click="toggleFullScreen()" class="cursor-pointer ab">
-            <img src="fullscreen.png" width="30" alt="" />
+          <span @click="toggleFullScreen()" class="cursor-pointer bg-gray-800 p-2   rounded-lg">
+            <img src="fullscreen.png" width="15" alt="" />
           </span>
         </div>
       </div>
@@ -98,50 +101,18 @@
 </template>
 
 <script>
-import TypingSvg from "../components/TypingSvg.vue";
+import Player from "../methods/Player";
 export default {
   name: "IndexPage",
-  components: {
-    TypingSvg,
-  },
   data() {
     return {
-      authUrl: "",
-      isMute: false,
-      showVolume: false,
-      showTyping: false,
-      showRaining: false,
-      isPlaying: false,
-      isTyping: false,
-      isRaining: false,
+      primary: new Player("primary"),
+      rain: new Player("rain"),
+      type: new Player("type"),
+
       isFullScreen: false,
-      volume: 0.5,
-      typing: 0.5,
-      raining: 0.5,
+      audio:"https://s3.us-east-2.amazonaws.com/lofi.co/lofi.co/tracks/chill/chill_4.mp3",
     };
-  },
-  watch: {
-    raining: {
-      handler() {
-        this.$refs.raining.play();
-        this.isRaining = true;
-        this.$refs.raining.volume = this.raining;
-      },
-    },
-    typing: {
-      handler() {
-        this.$refs.typing.play();
-        this.isTyping = true;
-        this.$refs.typing.volume = this.typing;
-      },
-    },
-    volume: {
-      handler() {
-        this.isMute = false;
-        this.$refs.player.muted = this.isMute;
-        this.$refs.player.volume = this.volume;
-      },
-    },
   },
   methods: {
     toggleFullScreen() {
@@ -152,35 +123,21 @@ export default {
       this.isFullScreen = !this.isFullScreen;
     },
     toggleMute() {
-      this.isMute = !this.isMute;
-      this.$refs.player.muted = this.isMute;
+      this.primary.muted = !this.primary.muted;
+      this.$refs.primary.muted = this.primary.muted;
     },
-    toggleRaining() {
-      if (this.isRaining) {
-        this.$refs.raining.pause();
-        this.isRaining = false;
-      } else {
-        this.$refs.raining.play();
-        this.isRaining = true;
-      }
+    slideVolume(player) {
+      this.$refs[player].play();
+      this[player].playing = true;
+      this.$refs[player].volume = this[player].volume;
     },
-    toggleTyping() {
-      this.$refs.typing.volume = 1;
-      if (this.isTyping) {
-        this.$refs.typing.pause();
-        this.isTyping = false;
+    playPause(player) {
+      if (this[player].playing) {
+        this.$refs[player].pause();
+        this[player].playing = false;
       } else {
-        this.$refs.typing.play();
-        this.isTyping = true;
-      }
-    },
-    togglePlay() {
-      if (this.isPlaying) {
-        this.$refs.player.pause();
-        this.isPlaying = false;
-      } else {
-        this.$refs.player.play();
-        this.isPlaying = true;
+        this.$refs[player].play();
+        this[player].playing = true;
       }
     },
   },
@@ -192,6 +149,5 @@ input[type="range"][orient="vertical"] {
   -webkit-appearance: slider-vertical; /* Chromium */
   width: 8px;
   height: 175px;
-  padding: 0 5px;
 }
 </style>
